de236e04162bc0b814884e17dd33b16831b9c7d5
jinglong <jinglong@eastmoney.com>
2022-02-15 14:58:39 +0800

    format the code

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int write_i = 0;
 atomic_bool done_flag;
 
 #define SING_TEST_NUM 1000000
-#define  THREAD_NUM 5
+#define THREAD_NUM 5
 #define THREAD_SEND_NUM 100000
 
 long long getCurrentTime()
@@ -42,23 +42,29 @@ long long getCurrentTime()
 
 void read_func(CMessageQueue *writeQueue, int threadId, const char *mes)
 {
-    while (1) {
+    while (1)
+    {
         BYTE data[100] = {0};
         int len = writeQueue->GetMessage(data);
-        if (len > 0) {
-            int i = atoi((const char *) data);
-            if (i != read_i && i != -1) {
+        if (len > 0)
+        {
+            int i = atoi((const char *)data);
+            if (i != read_i && i != -1)
+            {
                 printf("------------Read sequence error,i = %d,read = %s ------------\n", read_i, data);
                 writeQueue->PrintTrunk();
                 exit(-1);
             }
-            if (i == -1) {
+            if (i == -1)
+            {
                 break;
             }
             read_i++;
         }
-        else {
-            if (len != (int) eQueueErrorCode::QUEUE_NO_MESSAGE) {
+        else
+        {
+            if (len != (int)eQueueErrorCode::QUEUE_NO_MESSAGE)
+            {
                 printf("Read failed ret = %d\n", len);
                 writeQueue->PrintTrunk();
                 exit(-1);
@@ -66,35 +72,40 @@ void read_func(CMessageQueue *writeQueue, int threadId, const char *mes)
         }
     }
     printf("Read %s ,thread %d ,read count %d\n", mes, threadId, read_i);
-
 }
 
 void write_func(CMessageQueue *writeQueue, int threadId, const char *mes)
 {
-    while (1) {
-        if (write_i >= SING_TEST_NUM) {
+    while (1)
+    {
+        if (write_i >= SING_TEST_NUM)
+        {
             break;
         }
         const string &data = to_string(write_i);
-        int iRet = writeQueue->SendMessage((BYTE *) data.c_str(), data.length());
-        if (iRet == 0) {
+        int iRet = writeQueue->SendMessage((BYTE *)data.c_str(), data.length());
+        if (iRet == 0)
+        {
             write_i++;
         }
-        else {
-            if (iRet != (int) eQueueErrorCode::QUEUE_NO_SPACE) {
+        else
+        {
+            if (iRet != (int)eQueueErrorCode::QUEUE_NO_SPACE)
+            {
                 printf("Write failed data = %d,ret = %d\n", write_i, iRet);
                 writeQueue->PrintTrunk();
                 exit(-1);
             }
         }
-
     }
 
     //over
-    while (true) {
+    while (true)
+    {
         const string &data = to_string(-1);
-        int iRet = writeQueue->SendMessage((BYTE *) data.c_str(), data.length());
-        if (iRet == 0) {
+        int iRet = writeQueue->SendMessage((BYTE *)data.c_str(), data.length());
+        if (iRet == 0)
+        {
             break;
         }
     }
@@ -104,18 +115,23 @@ void write_func(CMessageQueue *writeQueue, int threadId, const char *mes)
 void mul_read_func(CMessageQueue *writeQueue, int threadId, const char *mes)
 {
     int i = 1;
-    while (1) {
+    while (1)
+    {
         BYTE data[100] = {0};
         int len = writeQueue->GetMessage(data);
-        if (len > 0) {
+        if (len > 0)
+        {
             i++;
             read_count++;
         }
-        else {
-            if (done_flag) {
+        else
+        {
+            if (done_flag)
+            {
                 break;
             }
-            if (len != (int) eQueueErrorCode::QUEUE_NO_MESSAGE) {
+            if (len != (int)eQueueErrorCode::QUEUE_NO_MESSAGE)
+            {
                 printf("Read failed ret = %d\n", len);
                 writeQueue->PrintTrunk();
                 exit(-1);
@@ -123,27 +139,32 @@ void mul_read_func(CMessageQueue *writeQueue, int threadId, const char *mes)
         }
     }
     printf("Read %s ,thread %d ,read count %d\n", mes, threadId, i);
-
 }
 
 void mul_write_func(CMessageQueue *writeQueue, int threadId, const char *mes)
 {
     int i = 0;
-    while (1) {
-        if (i >= THREAD_SEND_NUM) {
+    while (1)
+    {
+        if (i >= THREAD_SEND_NUM)
+        {
             break;
         }
         const string &data = to_string(i);
-        int iRet = writeQueue->SendMessage((BYTE *) data.c_str(), data.length());
-        if (iRet == 0) {
+        int iRet = writeQueue->SendMessage((BYTE *)data.c_str(), data.length());
+        if (iRet == 0)
+        {
             i++;
             write_count++;
-            if (write_count >= THREAD_SEND_NUM * THREAD_NUM) {
+            if (write_count >= THREAD_SEND_NUM * THREAD_NUM)
+            {
                 done_flag.store(true);
             }
         }
-        else {
-            if (iRet != (int) eQueueErrorCode::QUEUE_NO_SPACE) {
+        else
+        {
+            if (iRet != (int)eQueueErrorCode::QUEUE_NO_SPACE)
+            {
                 printf("Write failed data = %d,ret = %d\n", write_i, iRet);
                 writeQueue->PrintTrunk();
                 exit(-1);
@@ -163,16 +184,18 @@ void SingleRWTest()
     write_thread.join();
     long end = getCurrentTime();
     printf("=======================SingleRWTest=============================\n");
-    printf("SingleRWTest cost time %d ms\n", (int) (end - begin));
+    printf("SingleRWTest cost time %d ms\n", (int)(end - begin));
     printf("Read read_count %d \n", read_i);
     printf("Write write_count %d \n", write_i);
-    if (read_i == write_i) {
+    if (read_i == write_i)
+    {
         printf("SingleRWTest ok %d \n");
     }
-    else {
+    else
+    {
         printf("SingleRWTest failed %d \n");
     }
-    delete  messQueue;
+    delete messQueue;
     messQueue = nullptr;
 }
 
@@ -185,33 +208,39 @@ void MulRWTest()
     printf("===============================================================\n");
     long begin = getCurrentTime();
     list<thread> read;
-    for (int i = 0; i < THREAD_NUM; i++) {
+    for (int i = 0; i < THREAD_NUM; i++)
+    {
         read.push_back(move(thread(mul_read_func, messQueue, i, "MulRWTest")));
     }
     list<thread> write;
-    for (int i = 0; i < THREAD_NUM; i++) {
+    for (int i = 0; i < THREAD_NUM; i++)
+    {
         write.push_back(move(thread(mul_write_func, messQueue, i, "MulRWTest")));
     }
 
-    for (thread &thread : write) {
+    for (thread &thread : write)
+    {
         thread.join();
     }
 
-    for (thread &thread : read) {
+    for (thread &thread : read)
+    {
         thread.join();
     }
     long end = getCurrentTime();
     printf("=======================MulRWTest===============================\n");
-    printf("MulRWTest cost time %d ms\n", (int) (end - begin));
+    printf("MulRWTest cost time %d ms\n", (int)(end - begin));
     printf("Read read_count %d \n", read_count.load());
     printf("Write write_count %d \n", write_count.load());
-    if (read_count.load() == write_count.load()) {
+    if (read_count.load() == write_count.load())
+    {
         printf("MulRWTest ok\n");
     }
-    else {
+    else
+    {
         printf("MulRWTest failed\n");
     }
-    delete  messQueue;
+    delete messQueue;
     messQueue = nullptr;
 }
 
--- shm_rwlock.cpp
+++ shm_rwlock.cpp
@@ -28,218 +28,227 @@ struct sembuf {
 using namespace std;
 namespace shmmqueue
 {
-CShmRWlock::CShmRWlock()
-    : m_iSemID(-1), m_iSemKey(-1)
-{
-
-}
-
-CShmRWlock::CShmRWlock(key_t iKey)
-{
-    init(iKey);
-}
-
-void CShmRWlock::init(key_t iKey)
-{
-#if defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)
-    /* union semun is defined by including <sys/sem.h> */
-#else
-    /* according to X/OPEN we have to define it ourselves */
-    union semun
+    CShmRWlock::CShmRWlock()
+        : m_iSemID(-1), m_iSemKey(-1)
     {
-        int val;                  /* value for SETVAL */
-        struct semid_ds *buf;     /* buffer for IPC_STAT, IPC_SET */
-        unsigned short *array;    /* array for GETALL, SETALL */
-        /* Linux specific part: */
-        struct seminfo *__buf;    /* buffer for IPC_INFO */
-    };
+    }
+
+    CShmRWlock::CShmRWlock(key_t iKey)
+    {
+        init(iKey);
+    }
+
+    void CShmRWlock::init(key_t iKey)
+    {
+#if defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)
+        /* union semun is defined by including <sys/sem.h> */
+#else
+        /* according to X/OPEN we have to define it ourselves */
+        union semun
+        {
+            int val;               /* value for SETVAL */
+            struct semid_ds *buf;  /* buffer for IPC_STAT, IPC_SET */
+            unsigned short *array; /* array for GETALL, SETALL */
+            /* Linux specific part: */
+            struct seminfo *__buf; /* buffer for IPC_INFO */
+        };
 #endif
-    int iSemID;
-    union semun arg;
-    //包含两个信号量,第一个为写信号两，第二个为读信号两
-    u_short array[2] = {0, 0};
-    //生成信号量集
-    if ((iSemID = semget(iKey, 2, IPC_CREAT | IPC_EXCL | 0666)) != -1) {
-        arg.array = &array[0];
+        int iSemID;
+        union semun arg;
+        //包含两个信号量,第一个为写信号两，第二个为读信号两
+        u_short array[2] = {0, 0};
+        //生成信号量集
+        if ((iSemID = semget(iKey, 2, IPC_CREAT | IPC_EXCL | 0666)) != -1)
+        {
+            arg.array = &array[0];
 
-        //将所有信号量的值设置为0
-        if (semctl(iSemID, 0, SETALL, arg) == -1) {
-            throw std::runtime_error("semctlt error: " + string(strerror(errno)));
+            //将所有信号量的值设置为0
+            if (semctl(iSemID, 0, SETALL, arg) == -1)
+            {
+                throw std::runtime_error("semctlt error: " + string(strerror(errno)));
+            }
         }
+        else
+        {
+            //如果失败，判断信号量是否已经存在
+            if (errno != EEXIST)
+            {
+                throw std::runtime_error("sem has exist error: " + string(strerror(errno)));
+            }
+
+            //连接信号量
+            if ((iSemID = semget(iKey, 2, 0666)) == -1)
+            {
+                throw std::runtime_error("semget error : " + string(strerror(errno)));
+            }
+        }
+
+        m_iSemKey = iKey;
+        m_iSemID = iSemID;
     }
-    else {
-        //如果失败，判断信号量是否已经存在
-        if (errno != EEXIST) {
-            throw std::runtime_error("sem has exist error: " + string(strerror(errno)));
-        }
 
-        //连接信号量
-        if ((iSemID = semget(iKey, 2, 0666)) == -1) {
-            throw std::runtime_error("semget error : " + string(strerror(errno)));
-        }
-    }
-
-    m_iSemKey = iKey;
-    m_iSemID = iSemID;
-}
-
-int CShmRWlock::Rlock() const
-{
-    /*包含两个信号量,第一个为写信号量，第二个为读信号量
+    int CShmRWlock::Rlock() const
+    {
+        /*包含两个信号量,第一个为写信号量，第二个为读信号量
      *获取读锁
      *等待写信号量（第一个）变为0：{0, 0, SEM_UNDO},并且把读信号量（第一个）加一：{1, 1, SEM_UNDO}
      **/
-    struct sembuf sops[2] = {{0, 0, SEM_UNDO}, {1, 1, SEM_UNDO}};
-    size_t nsops = 2;
-    int ret = -1;
+        struct sembuf sops[2] = {{0, 0, SEM_UNDO}, {1, 1, SEM_UNDO}};
+        size_t nsops = 2;
+        int ret = -1;
 
-    do {
-        ret = semop(m_iSemID, &sops[0], nsops);
+        do
+        {
+            ret = semop(m_iSemID, &sops[0], nsops);
+        } while ((ret == -1) && (errno == EINTR));
+
+        return ret;
     }
-    while ((ret == -1) && (errno == EINTR));
 
-    return ret;
-}
+    int CShmRWlock::UnRlock() const
+    {
 
-int CShmRWlock::UnRlock() const
-{
-
-    /*包含两个信号量,第一个为写信号量，第二个为读信号量
+        /*包含两个信号量,第一个为写信号量，第二个为读信号量
      *解除读锁
      *把读信号量（第二个）减一：{1, -1, SEM_UNDO}
      **/
-    struct sembuf sops[1] = {{1, -1, SEM_UNDO}};
-    size_t nsops = 1;
+        struct sembuf sops[1] = {{1, -1, SEM_UNDO}};
+        size_t nsops = 1;
 
-    int ret = -1;
+        int ret = -1;
 
-    do {
-        ret = semop(m_iSemID, &sops[0], nsops);
+        do
+        {
+            ret = semop(m_iSemID, &sops[0], nsops);
 
+        } while ((ret == -1) && (errno == EINTR));
+
+        return ret;
     }
-    while ((ret == -1) && (errno == EINTR));
 
-    return ret;
-}
-
-bool CShmRWlock::TryRlock() const
-{
-    /*包含两个信号量,第一个为写信号量，第二个为读信号量
+    bool CShmRWlock::TryRlock() const
+    {
+        /*包含两个信号量,第一个为写信号量，第二个为读信号量
      *获取读锁
      *尝试等待写信号量（第一个）变为0：{0, 0,SEM_UNDO | IPC_NOWAIT},
      *把读信号量（第一个）加一：{1, 1,SEM_UNDO | IPC_NOWAIT}
      **/
-    struct sembuf sops[2] = {{0, 0, SEM_UNDO | IPC_NOWAIT}, {1, 1, SEM_UNDO | IPC_NOWAIT}};
-    size_t nsops = 2;
+        struct sembuf sops[2] = {{0, 0, SEM_UNDO | IPC_NOWAIT}, {1, 1, SEM_UNDO | IPC_NOWAIT}};
+        size_t nsops = 2;
 
-    int iRet = semop(m_iSemID, &sops[0], nsops);
-    if (iRet == -1) {
-        if (errno == EAGAIN) {
-            //无法获得锁
-            return false;
+        int iRet = semop(m_iSemID, &sops[0], nsops);
+        if (iRet == -1)
+        {
+            if (errno == EAGAIN)
+            {
+                //无法获得锁
+                return false;
+            }
+            else
+            {
+                throw std::runtime_error("semop error : " + string(strerror(errno)));
+            }
         }
-        else {
-            throw std::runtime_error("semop error : " + string(strerror(errno)));
-        }
+        return true;
     }
-    return true;
-}
 
-int CShmRWlock::Wlock() const
-{
-    /*包含两个信号量,第一个为写信号量，第二个为读信号量
+    int CShmRWlock::Wlock() const
+    {
+        /*包含两个信号量,第一个为写信号量，第二个为读信号量
      *获取写锁
      *尝试等待写信号量（第一个）变为0：{0, 0, SEM_UNDO},并且等待读信号量（第二个）变为0：{0, 0, SEM_UNDO}
      *把写信号量（第一个）加一：{0, 1, SEM_UNDO}
      **/
-    struct sembuf sops[3] = {{0, 0, SEM_UNDO}, {1, 0, SEM_UNDO}, {0, 1, SEM_UNDO}};
-    size_t nsops = 3;
+        struct sembuf sops[3] = {{0, 0, SEM_UNDO}, {1, 0, SEM_UNDO}, {0, 1, SEM_UNDO}};
+        size_t nsops = 3;
 
-    int ret = -1;
+        int ret = -1;
 
-    do {
-        ret = semop(m_iSemID, &sops[0], nsops);
+        do
+        {
+            ret = semop(m_iSemID, &sops[0], nsops);
 
+        } while ((ret == -1) && (errno == EINTR));
+
+        return ret;
     }
-    while ((ret == -1) && (errno == EINTR));
 
-    return ret;
-}
-
-int CShmRWlock::UnWlock() const
-{
-    /*包含两个信号量,第一个为写信号量，第二个为读信号量
+    int CShmRWlock::UnWlock() const
+    {
+        /*包含两个信号量,第一个为写信号量，第二个为读信号量
      *解除写锁
      *把写信号量（第一个）减一：{0, -1, SEM_UNDO}
      **/
-    struct sembuf sops[1] = {{0, -1, SEM_UNDO}};
-    size_t nsops = 1;
+        struct sembuf sops[1] = {{0, -1, SEM_UNDO}};
+        size_t nsops = 1;
 
-    int ret = -1;
+        int ret = -1;
 
-    do {
-        ret = semop(m_iSemID, &sops[0], nsops);
+        do
+        {
+            ret = semop(m_iSemID, &sops[0], nsops);
 
+        } while ((ret == -1) && (errno == EINTR));
+
+        return ret;
+
+        //return semop( m_iSemID, &sops[0], nsops);
     }
-    while ((ret == -1) && (errno == EINTR));
 
-    return ret;
-
-    //return semop( m_iSemID, &sops[0], nsops);
-
-}
-
-bool CShmRWlock::TryWlock() const
-{
-    /*包含两个信号量,第一个为写信号量，第二个为读信号量
+    bool CShmRWlock::TryWlock() const
+    {
+        /*包含两个信号量,第一个为写信号量，第二个为读信号量
      *尝试获取写锁
      *尝试等待写信号量（第一个）变为0：{0, 0, SEM_UNDO | IPC_NOWAIT},并且尝试等待读信号量（第二个）变为0：{0, 0, SEM_UNDO | IPC_NOWAIT}
      *把写信号量（第一个）加一：{0, 1, SEM_UNDO | IPC_NOWAIT}
      **/
-    struct sembuf sops[3] = {{0, 0, SEM_UNDO | IPC_NOWAIT},
-                             {1, 0, SEM_UNDO | IPC_NOWAIT},
-                             {0, 1, SEM_UNDO | IPC_NOWAIT}};
-    size_t nsops = 3;
+        struct sembuf sops[3] = {{0, 0, SEM_UNDO | IPC_NOWAIT},
+                                 {1, 0, SEM_UNDO | IPC_NOWAIT},
+                                 {0, 1, SEM_UNDO | IPC_NOWAIT}};
+        size_t nsops = 3;
 
-    int iRet = semop(m_iSemID, &sops[0], nsops);
-    if (iRet == -1) {
-        if (errno == EAGAIN) {
-            //无法获得锁
-            return false;
+        int iRet = semop(m_iSemID, &sops[0], nsops);
+        if (iRet == -1)
+        {
+            if (errno == EAGAIN)
+            {
+                //无法获得锁
+                return false;
+            }
+            else
+            {
+                throw std::runtime_error("semop error : " + string(strerror(errno)));
+            }
         }
-        else {
-            throw std::runtime_error("semop error : " + string(strerror(errno)));
-        }
+
+        return true;
     }
 
-    return true;
-}
+    int CShmRWlock::Lock() const
+    {
+        return Wlock();
+    }
 
-int CShmRWlock::Lock() const
-{
-    return Wlock();
-}
+    //Unlock 释放锁
+    int CShmRWlock::Unlock() const
+    {
+        return UnWlock();
+    }
 
-//Unlock 释放锁
-int CShmRWlock::Unlock() const
-{
-    return UnWlock();
-}
+    bool CShmRWlock::trylock() const
+    {
+        return TryWlock();
+    }
 
-bool CShmRWlock::trylock() const
-{
-    return TryWlock();
-}
+    //获取sem key
+    key_t CShmRWlock::Getkey() const
+    {
+        return m_iSemKey;
+    }
 
-//获取sem key
-key_t CShmRWlock::Getkey() const
-{
-    return m_iSemKey;
-}
-
-int CShmRWlock::getid() const
-//获取sem id
-{
-    return m_iSemID;
-}
+    int CShmRWlock::getid() const
+    //获取sem id
+    {
+        return m_iSemID;
+    }
 }
